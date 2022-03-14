1b5b5867f3c1385aafe63830a91c1897f11c90c7
Yassin <yassin.elwan@stud.fh-campuswien.ac.at>
2022-03-14 20:12:13 +0100

    zusammen mit luka grubisic und mohamad arastu

--- src/main/java/PasswordCtrl.java
+++ src/main/java/PasswordCtrl.java
@@ -1,5 +1,62 @@
 public class PasswordCtrl {
-    public static boolean checkPassword (String password){
+
+
+    public static boolean containsUpperCaseCharacter(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isUpperCase(string.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean containsLowerCaseCharacter(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLowerCase(string.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkLength(String password) {
         return password.length() >= 8 && password.length() <= 25;
     }
+
+    public static boolean checkIsDigit (String input){
+        for (char chr : input.toCharArray())
+            if (Character.isDigit(chr))
+                return true;
+
+        return false;
+    }
+
+    public static boolean checkIfSpecialCharacter (String input) {
+        char[] specialSymbols = new char[]{'@', '#', '$', '?', '!', '%', '/', '(', ')'};
+        for (char chr : input.toCharArray())
+            for (char chrArr : specialSymbols)
+                if (chr == chrArr)
+                    return true;
+        return false;
+    }
+
+
+
+
+
+
+
+    public static boolean checkPassword(String password) {
+
+        return checkLength(password) &&
+                containsLowerCaseCharacter(password) &&
+                containsUpperCaseCharacter(password) &&
+                checkIsDigit(password);
+
+
+    }
+
 }
--- src/test/java/PasswordCtrlTest.java
+++ src/test/java/PasswordCtrlTest.java
@@ -7,26 +7,103 @@ public class PasswordCtrlTest {
 
     @Test
     void checkPassowordTooShort() {
-        boolean actual = PasswordCtrl.checkPassword("1234567");
+        boolean actual = PasswordCtrl.checkLength("1234567");
         boolean expected = false;
         assertEquals(expected, actual);
     }
+
     @Test
     void checkPasswordCorrectLength() {
 
-        boolean actual = PasswordCtrl.checkPassword("12345678");
+        boolean actual = PasswordCtrl.checkLength("123456789");
+        boolean expected = true;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void checkPassowordTooLong() {
+
+        boolean actual = PasswordCtrl.checkLength("12345678123456781234567812345678");
+        boolean expected = false;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void checkPasswordUpperCase() {
+
+        boolean actual = PasswordCtrl.containsUpperCaseCharacter("KKKKKKKKKKKKKKKKK");
         boolean expected = true;
         assertEquals(expected, actual);
 
     }
     @Test
-    void checkPassowordTooLong() {
+    void checkPasswordLowerCase (){
 
-        boolean actual = PasswordCtrl.checkPassword("12345678123456781234567812345678");
+        boolean actual = PasswordCtrl.containsLowerCaseCharacter("kkkkkkkkkkkkkkkkkkkkk");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkPasswordLowerCaseFalse (){
+
+        boolean actual = PasswordCtrl.containsLowerCaseCharacter("KKKKKKKKKKKKKKKK");
+        boolean expected = false;
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void checkPasswordUpperCaseFalse () {
+
+        boolean actual = PasswordCtrl.containsUpperCaseCharacter("kkkkkkkkkkkkkkkkkk");
         boolean expected = false;
         assertEquals(expected, actual);
 
     }
 
+    @Test
+    void hasDigit (){
+        boolean actual = PasswordCtrl.checkIsDigit("123hshsus");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasNoDigit (){
+        boolean actual = PasswordCtrl.checkIsDigit("hshsussdsdsd");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasSpecialSymbol() {
+        boolean actual = PasswordCtrl.checkIfSpecialCharacter("das@!$2()");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void hasNoSpecialSymbol() {
+        boolean actual = PasswordCtrl.checkIfSpecialCharacter("das123");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void iscorrectPassword() {
+        boolean actual = PasswordCtrl.checkPassword("WasGehtAbBruder123@#!!");
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isNotCorrectPassword() {
+        boolean actual = PasswordCtrl.checkPassword("ksjdhwuwndi");
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
 
 }
